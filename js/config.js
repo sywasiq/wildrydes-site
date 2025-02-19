dfba4daf3b3ebcb3c24cf8e1b7ac0b2e335847b9
syeda <117393784+sywasiq@users.noreply.github.com>
2025-02-18 19:14:15 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_O9QteSebn', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '18ug8hbak2br282318a9ae5c4u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4c5gdvdnh9fairpluocfrok8ml', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
